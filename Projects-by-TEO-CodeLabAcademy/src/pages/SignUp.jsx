8bdb2f3d4208e12f915580634d675bd36f17acc9
Teo-iakovou <thedoros09@gmail.com>
2024-11-07 16:16:10 +0200

    Coders Application Project: Challenges Page

--- Projects-by-TEO-CodeLabAcademy/src/pages/SignUp.jsx
+++ Projects-by-TEO-CodeLabAcademy/src/pages/SignUp.jsx
@@ -8,7 +8,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-// Define Zod schema for validation
+// Zod schema for validation
 const signUpSchema = z.object({
   firstName: z.string().min(1, "First name is required"),
   lastName: z.string().min(1, "Last name is required"),
